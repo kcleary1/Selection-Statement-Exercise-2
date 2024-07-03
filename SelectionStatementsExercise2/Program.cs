a5c5d7a51be64311fe0e03753a36d7f6c7bb76fa
Kyle Cleary <kscleary11@gmail.com>
2024-07-02 20:53:32 -0600

    exercise complete

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is you favorite subject?");
+            
+            var subject = Console.ReadLine();
+
+
+
+            switch (subject)
+            {
+                case "algebra":
+                    Console.WriteLine("I always did well in math!");
+                    break;
+                case "science":
+                    Console.WriteLine("I always found science fascinating!");
+                    break;
+                case "music":
+                    Console.WriteLine("Music can very relaxing.");
+                    break;
+                case "gym":
+                    Console.WriteLine("It is always good to have physical exercise to get your blood flowing after sitting in class!");
+                    break;
+                case "literature":
+                    Console.WriteLine("A good book is always enjoyable!");
+                    break;
+                default:
+                    Console.WriteLine("I never took any classes in that subject, but it sounds interesting.");
+                    break;
+
+
+            }
         }
     }
 }
